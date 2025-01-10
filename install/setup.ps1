33de6d1b582d21bba75ecdf62be62a0e23e98339
monmejss <mbetsabe02@gmail.com>
2025-01-09 21:34:44 -0600

    Carpetas creadas

--- install/setup.ps1
+++ install/setup.ps1
@@ -23,7 +23,7 @@ function _colcon_prefix_chain_powershell_source_script {
 
 # source chained prefixes
 _colcon_prefix_chain_powershell_source_script "/opt/ros/iron\local_setup.ps1"
-_colcon_prefix_chain_powershell_source_script "/home/mariana/ros2_iron_ws/src/install\local_setup.ps1"
+_colcon_prefix_chain_powershell_source_script "/home/monica/ros2_iron/install\local_setup.ps1"
 
 # source this prefix
 $env:COLCON_CURRENT_PREFIX=(Split-Path $PSCommandPath -Parent)
